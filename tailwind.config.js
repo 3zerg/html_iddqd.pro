1f81aec0093a748a768af4ebe9ab60d7b26d4808
Valentin Cherkas <vc@iddqd.pro>
2023-02-22 14:01:47 +0300

    .

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "**/*.{js,html}",
+    "./src/**/*.{js,html}",
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    screens: {
+      sm: "600px",
+      md: "900px",
+      lg: "1200px",
+      xl: "1536px",
+    },
+    fontFamily: {
+      sans: ["GolosUIWebRegular", "sans-serif"],
+    },
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1rem" }],
+      base: ["1rem", { lineHeight: "1.15rem" }],
+      lg: ["1.125rem", { lineHeight: "1.25rem" }],
+      xl: ["1.25rem", { lineHeight: "1.5rem" }],
+      "2xl": ["1.5rem", { lineHeight: "1.5rem" }],
+      "3xl": ["2rem", { lineHeight: "2rem" }],
+    },
+  },
+  important: true,
+  darkMode: "class",
+  plugins: [require("@tailwindcss/typography")],
+}
